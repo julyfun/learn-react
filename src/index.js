8592e9ccc7f5b962396dd6b19f6a3629ce834edc
julyfun <43675484+julyfun@users.noreply.github.com>
2023-12-14 00:16:17 +0800

    3.2.2 支持 less

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
-import './common/style/frame.css'
+import './common/style/frame.less'
 
 ReactDom.render(<App />, document.getElementById('root'))
