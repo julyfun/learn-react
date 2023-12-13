ec6f970f84ff66f3c5cbe2e7fd1b9294a291768a
julyfun <43675484+julyfun@users.noreply.github.com>
2023-12-14 01:57:22 +0800

    4.1 页面构建,尚未设定路由

---
+++ src/src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './home.styl'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className='P-home'>
+                <h1>This is Survival Craft home page!</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ src/src/pages/login/index.js
@@ -0,0 +1,14 @@
+import React, { Componenet } from 'react'
+import './login.styl'
+
+class Login extends Componenet {
+    render() {
+        return (
+            <div className='P-login'>
+                <h1>This is Survival Craft login page!</h1>
+            </div>
+        )
+    }
+}
+
+export default Login
